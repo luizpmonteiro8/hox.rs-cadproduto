cd6757e327146d106aedc64596ac3a4d61353dd1
Luiz <luizpmonteiro8@hotmail.com>
2022-05-05 03:13:32 -0300

    removed cookie provider frontend

--- Frontend/src/pages/_app.tsx
+++ Frontend/src/pages/_app.tsx
@@ -6,21 +6,18 @@ import 'bootswatch/dist/cerulean/bootstrap.min.css';
 import { GlobalStyles } from '../styles/global-styles';
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-import { CookiesProvider } from 'react-cookie';
 import { Provider } from 'react-redux';
 import storeConfig from 'store/storeConfig';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextAuthProvider session={pageProps.session}>
-      <CookiesProvider>
-        <Provider store={storeConfig}>
-          <ThemeProvider theme={theme}>
-            <Component {...pageProps} />
-            <GlobalStyles />
-          </ThemeProvider>
-        </Provider>
-      </CookiesProvider>
+      <Provider store={storeConfig}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+          <GlobalStyles />
+        </ThemeProvider>
+      </Provider>
     </NextAuthProvider>
   );
 }
